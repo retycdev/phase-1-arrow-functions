bf4471c7cf99064087429e66961442c571ec8aed
Dan Kibaara <kibaara.gamedev@gmail.com>
2024-10-15 11:56:39 +0300

    Adds arrow functions

---
+++ index.js
@@ -0,0 +1,26 @@
+//write code here
+let a = 2000
+let b = 100
+
+
+//divide functn
+function divide(num1,num2){
+    return Math.abs(num1/num2)
+}
+
+//functn expression
+divide(a,b)
+
+//square  arrw functn
+const square=(num)=>{
+    return(num * num)
+}
+
+
+//add arrw functn
+const add=(a,b)=>{
+     a = 3
+    b = 4
+    return (a+b);
+
+}
